c13dbc4825a4a8ae554be23f43f84e2e4757ab91
unknown <longmap2003@gmail.com>
2024-10-30 14:52:44 +0700

    update navbar, cards, dao, model

--- saleapp/app/index.py
+++ saleapp/app/index.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import dao
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    cates = dao.load_categories()
+    prods = dao.load_products()
+    return render_template('index.html', categories=cates, products=prods)
 
 
 if __name__ == '__main__':
