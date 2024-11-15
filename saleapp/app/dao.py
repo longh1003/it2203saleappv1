f62f07fa0d0854a4658700f10346ca3fca900609
dhthanhqa@gmail.com <dhthanhqa@gmail.com>
2024-11-15 11:22:52 +0700

    login & register

--- saleapp/app/dao.py
+++ saleapp/app/dao.py
@@ -1,11 +1,14 @@
-from app.models import Category, Product
+from app.models import Category, Product, User
+from app import app, db
+import hashlib
+import cloudinary.uploader
 
 
 def load_categories():
     return Category.query.order_by('id').all()
 
 
-def load_products(kw=None, category_id=None):
+def load_products(kw=None, category_id=None, page=1):
     products = Product.query
 
     if kw:
@@ -14,4 +17,37 @@ def load_products(kw=None, category_id=None):
     if category_id:
         products = products.filter(Product.category_id == category_id)
 
+    page_size = app.config["PAGE_SIZE"]
+    start = (page - 1) * page_size
+    products = products.slice(start, start + page_size)
+
     return products.all()
+
+
+def count_products():
+    return Product.query.count()
+
+
+def auth_user(username, password):
+    password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+
+    return User.query.filter(User.username.__eq__(username.strip()),
+                             User.password.__eq__(password)).first()
+
+
+def add_user(name, username, password, avatar):
+    password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
+
+    u = User(name=name, username=username, password=password,
+             avatar='https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg')
+
+    if avatar:
+        res = cloudinary.uploader.upload(avatar)
+        u.avatar = res.get('secure_url')
+
+    db.session.add(u)
+    db.session.commit()
+
+
+def get_user_by_id(id):
+    return User.query.get(id)
